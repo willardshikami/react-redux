3505c0cd3a82daef111b775f34763098effa62cc
willardshikami <willardshikami@gmail.com>
2018-02-04 10:38:43 +0300

    router setup

---
+++ client/components/Single.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PhotoGrid from './PhotoGrid';
+
+class Single extends React.Component {
+  render() {
+    return (
+      <div className="single-photo">
+        I'm the single grid
+      </div>
+    );
+  }
+}
+
+export default Single;
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -6,13 +6,24 @@ import css from "./styles/style.styl";
 
 //import components
 import Main from "./components/Main";
+import Single from "./components/Single";
+import PhotoGrid from "./components/PhotoGrid";
 
+//react router dependencies
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
 
-
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+);
 
 
 
 ReactDOM.render(
-  <Main/>, 
+  router, 
   document.getElementById('root')
 );
