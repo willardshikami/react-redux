489f6f7a210aac3f9dd9632ca4184ae94b954ada
willardshikami <willardshikami@gmail.com>
2018-02-04 09:46:23 +0300

    added new components

---
+++ client/components/Main.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          <Link to="/">Reduxtagram</Link>
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Main;
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,18 @@
-// let's go!
+import React from "react";
+import ReactDOM from "react-dom";
+
+//import css
+import css from "./styles/style.styl";
+
+//import components
+import Main from "./components/Main";
+
+
+
+
+
+
+ReactDOM.render(
+  <Main/>, 
+  document.getElementById('root')
+);
